d0cf435dd0fc0c91676020400b8ef2ecf7a0e06a
glau-isidoro <glaucia.isidoro@gmail.com>
2017-07-04 22:14:54 -0300

    outra forma pro app.js

--- js/apppp.js
+++ js/apppp.js
@@ -1,33 +1,26 @@
-var cafes = [
-    {id: 01, title: 'Il Caffe', location:{lat: -23.5869946, lng: -46.6056592}},
-    {id: 02, title: 'Espaço Intuição - bendito café', location:{lat: -23.5930909, lng: -46.6063406}},
-    {id: 03, title: 'Sweet Lamour Café', location:{lat: -23.59494, lng: -46.6013998}},
-    {id: 04, title: 'Café Museu', location:{lat: -23.592377, lng: -46.6014334}},
-    {id: 05, title: 'Café Moinho', location:{lat: -23.5894519, lng: -46.6196099}},
+var places = [
+    {id: 01, type: 'cafe', title: 'Il Caffe', location:{lat: -23.5869946, lng: -46.6056592}},
+    {id: 02, type: 'cafe', title: 'Espaço Intuição - bendito café', location:{lat: -23.5930909, lng: -46.6063406}},
+    {id: 03, type: 'cafe', title: 'Sweet Lamour Café', location:{lat: -23.59494, lng: -46.6013998}},
+    {id: 04, type: 'cafe', title: 'Café Museu', location:{lat: -23.592377, lng: -46.6014334}},
+    {id: 05, type: 'cafe', title: 'Café Moinho', location:{lat: -23.5894519, lng: -46.6196099}},
+    {id: 06, type: 'restaurante', title: 'A Galeta Dourada', location:{lat: -23.588889, lng: -46.6111243}},
+    {id: 07, type: 'restaurante', title: 'Feijão de Corda', location:{lat: -23.5873994, lng: -46.6114217}},
+    {id: 08, type: 'restaurante', title: 'Magic Chicken', location:{lat: -23.5869897, lng: -46.6112558}},
+    {id: 09, type: 'restaurante', title: 'La Cumparsita', location:{lat: -23.5857109, lng: -46.6061113}},
+    {id: 10, type: 'restaurante', title: 'Novilho de Prata', location:{lat: -23.592112, lng: -46.6203427}},
+    {id: 11, type: 'bar', title: 'Sampa Jazz Bar', location:{lat: -23.5881649, lng: -46.6165315}},
+    {id: 12, type: 'bar', title: 'Boteco São Jorge', location:{lat: -23.5866931, lng: -46.6055235}},
+    {id: 13, type: 'bar', title: 'Venezas Bar', location:{lat: -23.5867904, lng: -46.6064541}},
+    {id: 14, type: 'bar', title: 'Beer Rock Club', location:{lat: -23.5910716, lng: -46.6072403}},
+    {id: 15, type: 'bar', title: 'Coronel Santinho', location:{lat: -23.5993395, lng: -46.6131555}},
 ];
 
-var restaurantes = [
-    {id: 06, title: 'A Galeta Dourada', location:{lat: -23.588889, lng: -46.6111243}},
-    {id: 07, title: 'Feijão de Corda', location:{lat: -23.5873994, lng: -46.6114217}},
-    {id: 08, title: 'Magic Chicken', location:{lat: -23.5869897, lng: -46.6112558}},
-    {id: 09, title: 'La Cumparsita', location:{lat: -23.5857109, lng: -46.6061113}},
-    {id: 10, title: 'Novilho de Prata', location:{lat: -23.592112, lng: -46.6203427}},
-];
-
-var bares = [
-    {id: 11, title: 'Sampa Jazz Bar', location:{lat: -23.5881649, lng: -46.6165315}},
-    {id: 12, title: 'Boteco São Jorge', location:{lat: -23.5866931, lng: -46.6055235}},
-    {id: 13, title: 'Venezas Bar', location:{lat: -23.5867904, lng: -46.6064541}},
-    {id: 14, title: 'Beer Rock Club', location:{lat: -23.5910716, lng: -46.6072403}},
-    {id: 15, title: 'Coronel Santinho', location:{lat: -23.5993395, lng: -46.6131555}},
-];
-
-var cafeMarkers = [];
-var restaurantMarkers = [];
-var barMarkers = [];
+var markers = [];
 
 var Place = function(data) {
     this.id = ko.observable(data.id);
+    this.type = ko.observable(data.type);
     this.title = ko.observable(data.title);
     this.location = ko.observable(data.location);
 };
@@ -42,16 +35,17 @@ var ViewModel = function() {
         return this.cafeList().concat(this.restaurantList(), this.barList());
     }, this);
 
-    cafes.forEach(function(cafeItem){
-        self.cafeList.push(new Place(cafeItem));
-    });
-
-    restaurantes.forEach(function(restaurantList){
-        self.restaurantList.push(new Place(restaurantList));
-    });
-
-    bares.forEach(function(barItem){
-        self.barList.push(new Place(barItem));
+    places.forEach(function(place){
+        switch (place.type) {
+            case 'cafe':
+                self.cafeList.push(new Place(place));
+                break;
+            case 'restaurante':
+                self.restaurantList.push(new Place(place));
+                break;
+            default:
+                self.barList.push(new Place(place));
+        }
     });
 
     this.createMarkers = function(markersList, locations, bounds, largeInfoWindow) {
@@ -59,11 +53,13 @@ var ViewModel = function() {
             var position = locations[i].location();
             var title = locations[i].title();
             var id = locations[i].id();
+            var type = locations[i].type();
             var marker = new google.maps.Marker({
                 position: position,
                 title: title,
                 animation: google.maps.Animation.DROP,
-                id: id
+                id: id,
+                type: type
             });
             markersList.push(marker);
             bounds.extend(marker.position);
@@ -116,10 +112,14 @@ var ViewModel = function() {
         map.fitBounds(bounds);
     }
 
-    this.hideListings = function(list) {        
+    this.hideListings = function(list) {
         for(var i = 0 ; i < list.length ; i++) {
             list[i].setMap(null);
         }
+    }
+
+    this.showMarkers = function(list) {
+        debugger
     }
 
     $('.button-collapse').sideNav({
@@ -147,9 +147,7 @@ function initMap() {
     var largeInfoWindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
 
-    viewModel.createMarkers(barMarkers, viewModel.barList(), bounds, largeInfoWindow);
-    viewModel.createMarkers(restaurantMarkers, viewModel.restaurantList(), bounds, largeInfoWindow);
-    viewModel.createMarkers(cafeMarkers, viewModel.cafeList(), bounds, largeInfoWindow);
+    viewModel.createMarkers(markers, viewModel.allLists(), bounds, largeInfoWindow);
 
 }
 
